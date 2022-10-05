bb190107a46e1b918e036745b1c09b87cda81513
vaibhavmalhotra002 <vaibhavmalhotra002@gmail.com>
2022-10-05 19:07:54 +0530

    commit changes

--- first.js
+++ first.js
@@ -1,5 +1,6 @@
 console.log("My first javascript code")
-let a=78 //let is used to declare a variable in javascript
+let a=78
+a = "vaibhav" //let is used to declare a variable in javascript
 //a contains 78
 //copywriting
 console.log(a)
